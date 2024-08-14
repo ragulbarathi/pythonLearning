c402ecd09f8b747165ea7f44a48569404f398b1c
ragulbarathi <ragulbarathi98@gmail.com>
2024-08-14 10:14:31 +0530

    String slicing

---
+++ src/pythonConcepts/String slicing.py
@@ -0,0 +1,13 @@
+my_string = "Hello, World!"
+
+# Extract from index 0 to 4 (exclusive)
+print(my_string[0:5])  # Output: Hello
+
+# Extract from index 7 to the end
+print(my_string[7:])  # Output: World!
+
+# Extract every other character
+print(my_string[::2])  # Output: Hlo ol!
+
+# Reverse the string
+print(my_string[::-1])  # Output: !dlroW ,olleH
