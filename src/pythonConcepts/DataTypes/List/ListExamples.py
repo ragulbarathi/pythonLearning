b58ac846032a1f6889456eb4535133a1a9b11ad4
ragulbarathi <ragulbarathi98@gmail.com>
2024-08-15 09:18:18 +0530

    Pythons concepts updated

---
+++ src/pythonConcepts/DataTypes/List/ListExamples.py
@@ -0,0 +1,29 @@
+List = []
+print("Initial blank List: ")
+print(List)
+List = ['GeeksForGeeks']
+print("\nList with the use of String: ")
+print(List)
+List = ["Geeks", "For", "Geeks"]
+print("\nList containing multiple values: ")
+print(List[0])
+print(List[2])
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Geeks', 'For'], ['Geeks']]
+
+# accessing an element from the
+# Multi-Dimensional List using
+# index number
+print("Accessing a element from a Multi-Dimensional list")
+print(List[0][1], List[0][0])
+print(List[1][0])
+
+# Creating a List
+List1 = []
+print(len(List1))
+
+# Creating a List of numbers
+List2 = [10, 20, 14]
+print(len(List2))
---
+++ src/pythonConcepts/DataTypes/List/takinginputlist.py
@@ -0,0 +1,11 @@
+# Python program to take space
+# separated input as a string
+# split and store it to a list
+# and print the string list
+
+# input the list as string
+string = input("Enter elements (Space-Separated): ")
+
+# split the strings and store it to a list
+lst = string.split()
+print('The list is:', lst)   # printing the list
