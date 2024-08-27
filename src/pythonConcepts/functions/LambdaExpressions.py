96ea784f8b394f07f3fdb39b654487c95c11b727
ragulbarathi <ragulbarathi98@gmail.com>
2024-08-27 09:03:31 +0530

    Functions-Lambda expressions           PythonDecorators

---
+++ src/pythonConcepts/functions/LambdaExpressions.py
@@ -0,0 +1,6 @@
+"""Python Lambda Functions are anonymous functions means that the function is without a name.
+As we already know the def keyword is used to define a normal function in Python. Similarly,
+the lambda keyword is used to define an anonymous function in Python. """
+
+add = lambda a, b: a + b
+print(add(2, 3))
